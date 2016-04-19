19f637f4a1fe201fc904197b40d77254faa21cb6
machlabd <machlabd@users.noreply.github.com>
2016-04-19 16:43:42 +0200

    Update 3_DAMEfinder.R

--- real_data/R/3_DAMEfinder.R
+++ real_data/R/3_DAMEfinder.R
@@ -19,12 +19,30 @@ pos2 <- as.numeric(limma::strsplit2(rows, ".", fixed=T)[,3])
 midpt <- floor((pos2 - pos1)/2)
 pos <- pos1 + midpt
 
+# moderated t-statistic
+# mod is the design matrix. 0 is for normal, 1 is for adenoma
+mod <- matrix(data=c(rep(1,13), c(0,0,1,1,1,1,1,1,1,1,1,0,1)), ncol = 2)
+coeff <- 2 # the column in the design matrix to consider
 
+fit <- limma::lmFit(sm_t, mod)
 
+fit2 <- limma::eBayes(fit, proportion = 0.01)
+beta <- fit2$t[, coeff]
 
+# smoothing: by clusters on the genome called pns
+# smoothing functions from bumphunter: loessByCluster and runmedByCluster
 
+pns <- clusterMaker(chr, pos, maxGap = 300)
 
+verbose <- TRUE
+Q <- 0.9
 
+smooth <- smoother(y = beta, x = pos, cluster = pns, smoothFunction = loessByCluster, 
+                  verbose = verbose)
+smoothed_beta <- smooth$fitted
 
+# Detect DAMEs
+dames <- regionFinder(x = smoothed_beta, chr = chr, pos = pos, cluster = pns, 
+                      cutoff = quantile(abs(smoothed_beta),Q, na.rm=T), verbose = verbose)
 
-
+save(dames, file="real_DAMEs.rda")
