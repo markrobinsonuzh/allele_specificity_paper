238cd6db2e5da39a20bbe748a6da7b284d01a7a8
machlabd <machlabd@users.noreply.github.com>
2016-04-19 16:33:24 +0200

    Update 3_DAMEfinder.R

--- real_data/R/3_DAMEfinder.R
+++ real_data/R/3_DAMEfinder.R
@@ -1,1 +1,22 @@
 # We use the sm_t matrix which contains the transformed ASM scores and the bump hunting methods to detect DAMEs.
+
+# setwd("/home/Shared_taupo/data/seq/bisulphite_mirco/FASTQ/trimmed_t20l20/bump_hunting/full_bumphunting_real_data/with_bumphunter_package/")
+
+# load sm_t
+# load("real_sm_transformed.rda")
+
+library(limma)
+library(bumphunter)
+
+############################################################
+
+
+
+
+
+
+
+
+
+
+
