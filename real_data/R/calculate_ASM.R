9521c2f145e8c839dfe30289ffb11824b640db25
machlabd <machlabd@users.noreply.github.com>
2016-04-19 11:40:42 +0200

    Update calculate_ASM.R

--- real_data/R/calculate_ASM.R
+++ real_data/R/calculate_ASM.R
@@ -1,2 +1,15 @@
 # We calculate the ASM score for each tuple across the samples and transform the ASM scores with a square root transformation.
 
+library(parallel)
+library(data.table)
+library(LearnBayes)
+
+# setwd("/home/Shared/data/seq/bisulphite_mirco/FASTQ/trimmed_t20l20/bump_hunting/full_bumphunting_real_data/with_bumphunter_package")
+
+########################################################################
+
+# load("real_methtuple_list_o_final.rda")
+
+
+
+
