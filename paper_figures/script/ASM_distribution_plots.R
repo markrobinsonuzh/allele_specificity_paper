a333ed2b471d648994923e99fc6662903e514872
machlabd <machlabd@users.noreply.github.com>
2016-06-27 00:12:36 +0200

    Create ASM_distribution_plots.R

---
+++ paper_figures/script/ASM_distribution_plots.R
@@ -0,0 +1,8 @@
+## Figure: 3 - ASM distribution plot
+## Data: real data
+## What we do: Here we plot the distribution of the ASM score. We show the distribution for all samples for autosomal chr, chrY, chrX, and chrMT
+
+# load("autosomal_df_ASM.rda")
+# load("chrY_df_ASM.rda")
+# load("chrX_df_ASM.rda")
+# load("chrMT_df_ASM.rda")
