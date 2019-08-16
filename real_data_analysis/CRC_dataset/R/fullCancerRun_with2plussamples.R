cdeb8159b64241d4821c5999f79c5fbb7e0b62e5
sorjuela <sorjuela@gmail.com>
2019-08-16 15:39:40 +0200

    Add some code to test ASMstat, make contrasts for that, MDS fixes for paper, update bw function

--- real_data_analysis/CRC_dataset/R/fullCancerRun_with2plussamples.R
+++ real_data_analysis/CRC_dataset/R/fullCancerRun_with2plussamples.R
@@ -25,10 +25,10 @@ save(rds,file="data/extracted_bams.RData")
 derASM <- calc_derivedasm(rds)
 
 #save(derASM, file = "../data/derASM_fullCancer.RData")
-load("data/derASM_fullCancer.RData")
+load("data/derASM_fullcancer2.RData")
 
-derASM <- GenomeInfoDb::sortSeqlevels(derASM) #only necessary for old calc_derivedasm()
-derASM <- sort(derASM)
+#derASM <- GenomeInfoDb::sortSeqlevels(derASM) #only necessary for old calc_derivedasm()
+#derASM <- sort(derASM)
 
 #Filter
 filt <- rowSums(!is.na(assay(derASM, "der.ASM"))) >= 10
@@ -36,7 +36,6 @@ derASM <- derASM[filt,]
 
 #Plot checks
 myColor <- RColorBrewer::brewer.pal(9, "Set1")[c(2,3,5,8)]
-m1 <- methyl_MDS_plot(derASM, group = metadata$V2, color = myColor)
 
 x <- assay(derASM,"der.ASM")
 #xsub <- x[,grp %in% c("CRC_cimp","NORM_cimp")]
@@ -56,12 +55,31 @@ ggplot(dd, aes(means, var)) + geom_point(alpha = 0.2) + theme_bw()
 grp <- factor(metadata$V2)
 grp <- relevel(grp, "NORM_cimp")
 samp <- gsub("CRC|NORM","", metadata$V1)
-mod <- model.matrix(~grp+samp)
+mod <- model.matrix(~0+grp+samp)
 mod <- mod[,-9] #because Coefficients not estimable: samp6_cimp
+cont <- limma::makeContrasts(grpCRC_cimp-grpNORM_cimp, grpCRC_non-grpNORM_non, 
+                             levels = mod)
 
 #Get DAMEs
-dames_noncimp <- find_dames(derASM, mod, coef = 2, maxGap = 100)
-dames_noncimp <- find_dames(derASM, mod, coef = 2, pvalAssign = "empirical", maxGap = 100) 
+dames_cimp <- find_dames(derASM, mod, coef = 1, contrast = cont, maxGap = 100)
+dames_noncimp <- find_dames(derASM, mod, coef = 2, contrast = cont, maxGap = 100)
+
+#perms
+dames_cimp <- find_dames(derASM, mod, coef = 1, contrast = cont, 
+                            pvalAssign = "empirical", maxGap = 100) 
+
+#use ASMstat
+refmeth <- assay(derASM, "ref.meth")
+altmeth <- assay(derASM, "alt.meth")
+
+refcov <- assay(derASM, "ref.cov")
+altcov <- assay(derASM, "alt.cov")
+
+prop <- (refmeth+altmeth)/(refcov+altcov)
+
+ASMstat <- ((refmeth/refcov) - (altmeth/altcov)) /
+  sqrt(prop * (1 - prop) * ((1/refcov) + (1/altcov)))
+assay(derASM, "der.ASM") <- abs(ASMstat) #and run the above
 
 #Plot methyl_circles 
 snp2 <- GRanges(9, IRanges(99983847, width = 1)) #este 
@@ -83,8 +101,8 @@ m2 <- methyl_circle_plot(snp2, vcfFile = gsub("/home/",path,metadata$V4[8]),
                    dame = dame,
                    pointSize = 2)
 
-m4 <- cowplot::plot_grid(m2,m1, ncol=1, nrow = 2, labels = c("A","B"))
-ggplot2::ggsave("MCircle_plots/MethylcirclesSNP.png", m4, width = 12, height = 10)
+m4 <- cowplot::plot_grid(m2,m1, ncol=1, nrow = 2, labels = c("NORM","CRC"))
+ggplot2::ggsave("MCircle_plots/MethylcirclesSNP_fullmapq.png", m4, width = 12, height = 10)
 
 
 
@@ -101,14 +119,18 @@ filt <- c(rowSums(assay(ASM, "cov") >= 10 & !is.na(assay(ASM, "cov"))) >= 10)
 ASM <- ASM[filt,] #2,015,001, 1,849,831
 
 #MDS
-m2 <- methyl_MDS_plot(ASM, group = metadata$V2, color = myColor, adj = 0.03)
+colnames(derASM) <- c(paste0("C",1:6),paste0("N",1:6))
+colnames(ASM) <- c(paste0("C",1:6),paste0("N",1:6))
+m1 <- methyl_MDS_plot(derASM, group = metadata$V2, color = myColor, pointSize = 6, adj = 0.04) +
+  theme(legend.position = "none")
+m2 <- methyl_MDS_plot(ASM, group = metadata$V2, color = myColor, pointSize = 6, adj = 0.05) +
+  theme(legend.position = "none")
 
-#full paper figure TODO: fix for new MDS function
-m1 <- m1 + theme(text = element_text(size = 15))
-m2 <- m2 +  theme(legend.position = "none", text = element_text(size = 15))
+#Paper figure
 m4 <- cowplot::plot_grid(m2,m1, ncol=2, nrow = 2, labels = c("A","B","C"),
-                         rel_widths = c(1,1.3), rel_heights = 1)
-ggplot2::ggsave("curvesNscatters/MDSboth.png", m4, width = 12, height = 10)
+                         rel_widths = c(1,1), rel_heights = 1)
+
+ggplot2::ggsave("curvesNscatters/MDSboth.png", m4, width = 8, height = 6)
 
 #MD and MV plots
 x <- assay(ASM,"asm")
@@ -159,16 +181,18 @@ m2 <- methyl_circle_plotCpG(trimmedMEG3,
                             pointSize = 1,
                             order = TRUE)
 
-m4 <- cowplot::plot_grid(m2,m1, ncol=1, nrow = 2, labels = c("A","B"))
-ggplot2::ggsave("MCircle_plots/methylCirclesTopTupleDAMECIMP.png", m4, width = 10, 
+m4 <- cowplot::plot_grid(m2,m1, ncol=1, nrow = 2, labels = c("NORM","CRC"))
+ggplot2::ggsave("MCircle_plots/MethylcirclesCpG_topDAME.png", m4, width = 10, 
                 height = 12)
 
 #### build bigwigs ####
 
-sapply(colnames(derASM), make_bigwig, score.obj = derASM, folder =".." , 
-       chromsizes.file = "/home/Shared_taupo/steph/reference/hg19.chrom.sizes.mod")
+sapply(colnames(derASM), make_bigwig, scoreObj = derASM, folder =".." , 
+       chromsizesFile = "/home/Shared_taupo/steph/reference/hg19.chrom.sizes.mod")
 
-sapply(colnames(ASM), make_bigwig, score.obj = ASM, folder =".." , 
-       chromsizes.file = "/home/Shared_taupo/steph/reference/hg19.chrom.sizes.mod")
+sapply(colnames(ASM), make_bigwig, scoreObj = ASM, folder =".." , 
+       chromsizesFile = "/home/Shared_taupo/steph/reference/hg19.chrom.sizes.mod")
 
-
+#with ASMstat
+sapply(colnames(derASM), make_bigwig, scoreObj = derASM, folder ="../.." , 
+       chromsizesFile = "/home/Shared_taupo/steph/reference/hg19.chrom.sizes.mod")
